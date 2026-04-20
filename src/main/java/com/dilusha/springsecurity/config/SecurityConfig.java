7fff5d267bdfc9bc2f71332e421a84418d45bdc6
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-20 19:52:42 +0530

    Disabling CSRF Token

--- src/main/java/com/dilusha/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/dilusha/springsecurity/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.dilusha.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +14,11 @@ public class SecurityConfig {
 
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-       return http.build();
+        return http.build();
     }
 }
